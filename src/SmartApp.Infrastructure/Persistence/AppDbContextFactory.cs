9b3b5fbc451824d427dcc50b9457dbe75badfd84
Nayeem Bhuiyan <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2026-05-30 22:25:47 +0600

    setting change

--- src/SmartApp.Infrastructure/Persistence/AppDbContextFactory.cs
+++ src/SmartApp.Infrastructure/Persistence/AppDbContextFactory.cs
@@ -9,7 +9,7 @@ public sealed class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbConte
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
         optionsBuilder.UseSqlServer(
-            "Server=localhost,1433;Database=SmartAppDb;User Id=sa;Password=Your_password123;TrustServerCertificate=True;",
+            "Server=localhost,1433;Database=SmartAppDb;User Id=sa;Password=NayeemDev2026@Secure#Pass01;TrustServerCertificate=True;",
             sql => sql.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
 
         return new AppDbContext(optionsBuilder.Options);
